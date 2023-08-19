f10bdf864c9313175fcb772827ccc3e4c7efcfab
Jhonatan Kuns <jhonatanksantos047@gmail.com>
2023-08-19 17:51:44 -0300

    Criação do arquivo text_configs
    Arquivo que será responsável por mostrar os prints no terminal de forma organizada e em cores.
    
    Atualmente funções criadas:
    
    -- color() -> Responsável por manipular as cores usando a lib "colorama"
    
    -- titulos() -> Responsável por manipular a frase "msg" em formato de título.

---
+++ game_nsx/configs_game/text_configs.py
@@ -0,0 +1,71 @@
+from colorama import Fore, Style, Back, init
+init(autoreset=True)
+
+
+def color(text, sit):
+    """Alterar a cor da frase/texto
+
+    Args:
+        text (STRING): Aqui será definido o texto/mensagem que o usuário irá colocar cores.
+        sit (STRING): Definir a cor que será usada.
+            Cores disponíveis:
+            
+                - red  |  lred
+                - green  |  lgreen
+                - blue  |  lblue
+                - magenta  |  lmagenta
+                - cyan  |  lcyan
+                - yellow  |  lyellow
+                - white  |  lwhite
+                
+            As cores que possuem o " l " antes do tipo da cor é para indicar que a cor é mais clara
+    """
+    sit = str(sit).lower()
+#TODO......................NORMAL COLORS......................
+
+    if sit == "red":
+        return f"{Fore.RED+Style.BRIGHT}{text}"
+    elif sit == "green":
+        return f"{Fore.GREEN+Style.BRIGHT}{text}"
+    elif sit == "blue":
+        return f"{Fore.BLUE+Style.BRIGHT}{text}"
+    elif sit == "magenta":
+        return f"{Fore.MAGENTA+Style.BRIGHT}{text}"
+    elif sit == "cyan":
+        return f"{Fore.CYAN+Style.BRIGHT}{text}"
+    elif sit == "yellow":
+        return f"{Fore.YELLOW+Style.BRIGHT}"
+    elif sit == "white":
+        return f"{Fore.WHITE+Style.BRIGHT}{text}"
+    
+#TODO......................LIGHT COLORS......................
+
+    elif sit == "lred":
+        return f"{Fore.LIGHTRED_EX+Style.BRIGHT}{text}"
+    elif sit == "lgreen":
+        return f"{Fore.LIGHTGREEN_EX+Style.BRIGHT}{text}"
+    elif sit == "lblue":
+        return f"{Fore.LIGHTBLUE_EX+Style.BRIGHT}{text}"
+    elif sit == "lmagenta":
+        return f"{Fore.LIGHTMAGENTA_EX+Style.BRIGHT}{text}"
+    elif sit == "lcyan":
+        return f"{Fore.LIGHTCYAN_EX+Style.BRIGHT}{text}"
+    elif sit == "lyellow":
+        return f"{Fore.LIGHTYELLOW_EX+Style.BRIGHT}"
+    elif sit == "lwhite":
+        return f"{Fore.LIGHTWHITE_EX+Style.BRIGHT}{text}"
+
+
+def titulos(msg):
+    """FORMATAR TÍTULOS
+
+    Args:
+        msg (STRING): Mensagem que irá formatar em formato TÍTULO
+    """
+    msg = str(msg)
+    width = 60
+    print()
+    print("-~" * 30)
+    print(f"{msg.center(width)}")
+    print("-~" * 30)
+    print()
